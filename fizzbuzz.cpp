977faa6abbb902d16f35e377507f28c56ad81823
Standard8989 <NewStadard.first@gmail.com>
2022-12-07 00:28:14 +0900

    かんせー

---
+++ fizzbuzz.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+#define FIZZ 3
+#define BUZZ 5
+
+int main() {
+    int count = 1;
+
+    while (true) {
+        if (count % (FIZZ * BUZZ))
+            cout << "FizzBuzz" << endl;
+        else if (count % FIZZ)
+            cout << "Fizz" << endl;
+        else if (count % BUZZ)
+            cout << "Buzz" << endl;
+        else
+            cout << count << endl;
+    }
+
+    return 0;
+}
